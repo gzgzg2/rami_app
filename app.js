403a8b5d55c12f3ff7f67ff0aadab0162ca0bdd2
devilMin <gzgzg2@naver.com>
2020-06-17 00:36:29 +0900

    commit commit commit

--- app.js
+++ app.js
@@ -2,8 +2,38 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", 'ejs');
+app.use(express.static(path.join(__dirname, 'views')));
+
 console.log(__dirname);
+
+
+var data={count:0};
+
+app.get('/', function (req, res) {
+  data.count++;
+  res.render('my_first_ejs', data);
+});
+
+app.get('/reset', function (req, res) {
+  data.count=0;
+  res.render('my_first_ejs', data);
+});
+
+app.get('/set/count', function (req, res) {
+  if(req.query.count) data.count = req.query.count;
+  res.render('my_first_ejs', data);
+});
+
+app.get('/set/:num', function (req, res) {
+  data.count = req.params.num;
+  res.render('my_first_ejs', data);
+});
+
+app.get('/', function(req, res){
+  res.render('my_first_ejs');
+});
+
 app.listen(5000, function () {
   console.log('Server On!');
 });
