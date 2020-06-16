4381ec0dee66458a2b6e8759731b4bb715f25211
devilMin <gzgzg2@naver.com>
2020-06-16 21:25:26 +0900

    commit commit

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
-app.use(express.static(path.join(__dirname + 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 console.log(__dirname);
 app.listen(5000, function () {
   console.log('Server On!');
