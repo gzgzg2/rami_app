060c5a27df766ab6833870eab328d4fd9a5330bb
devilMin <gzgzg2@naver.com>
2020-06-16 18:02:17 +0900

    commit을 하자

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ var express = require('express');
 
 var app = express();
 
-app.get('/',function (req, res) {
-  res.send('Hello World!');
-});
-
+app.use(express.static(path.join(__dirname + 'public')));
+console.log(__dirname);
 app.listen(5000, function () {
   console.log('Server On!');
 });
